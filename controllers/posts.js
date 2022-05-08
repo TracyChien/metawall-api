c3b19a2b1d1cc3b4dc79f87a100bcb04e43d485f
Tracy.C <tyringpood@gmail.com>
2022-05-09 00:19:10 +0800

    feat:新增錯誤處理(appError, handleErrorAsync)

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,10 @@
 const Post = require("../models/postsModel");
 const User = require("../models/usersModel");
-const handleError = require("../service/handleError");
+// const handleError = require("../service/handleError");
+const appError = require("../service/appError");
 
 const posts = {
-  async getPosts(req, res) {
+  async getPosts(req, res, next) {
     const timeSort = req.query.timeSort == "asc" ? "createdAt" : "-createdAt";
     const q =
       req.query.q !== undefined ? { content: new RegExp(req.query.q) } : {};
@@ -18,62 +19,57 @@ const posts = {
       data: posts,
     });
   },
-  async createdPost(req, res) {
-    try {
-      const data = req.body;
-      const newPost = await Post.create({
-        user: data.user,
+  async createdPost(req, res, next) {
+    const data = req.body;
+    if (data.content == undefined) {
+      return next(appError(400, "你沒有填寫 content 資料", next));
+    }
+    const newPost = await Post.create({
+      user: data.user,
+      content: data.content,
+      image: data.image,
+    });
+    res.status(200).json({
+      status: "success",
+      data: newPost,
+    });
+  },
+  async updatedPost(req, res, next) {
+    const id = req.params.id;
+    const data = req.body;
+    if (data.content == undefined) {
+      return next(appError(400, "你沒有填寫 content 資料", next));
+    }
+    const newPost = await Post.findByIdAndUpdate(
+      id,
+      {
+        name: data.name,
         content: data.content,
-      });
+      },
+      { new: true }
+    );
+    if (newPost !== null) {
       res.status(200).json({
         status: "success",
         data: newPost,
       });
-    } catch (error) {
-      handleError(res, error);
+    } else {
+      return next(appError(400, "查無此id", next));
     }
   },
-  async updatedPost(req, res) {
-    try {
-      const id = req.params.id;
-      const data = req.body;
-      const newPost = await Post.findByIdAndUpdate(
-        id,
-        {
-          name: data.name,
-          content: data.content,
-        },
-        { new: true }
-      );
-      if (newPost !== null) {
-        res.status(200).json({
-          status: "success",
-          data: newPost,
-        });
-      } else {
-        handleError(res, "");
-      }
-    } catch (err) {
-      handleError(res, err);
+  async deletedPost(req, res, next) {
+    const id = req.params.id;
+    const delPost = await Post.findByIdAndDelete(id);
+    if (delPost !== null) {
+      res.status(200).json({
+        status: "success",
+        data: delPost,
+      });
+    } else {
+      return next(appError(400, "查無此id", next));
     }
   },
-  async deletedPost(req, res) {
-    try {
-      const id = req.params.id;
-      const delPost = await Post.findByIdAndDelete(id);
-      if (delPost !== null) {
-        res.status(200).json({
-          status: "success",
-          data: delPost,
-        });
-      } else {
-        handleError(res, "");
-      }
-    } catch (err) {
-      handleError(res, err);
-    }
-  },
-  async clearPosts(req, res) {
+  async clearPosts(req, res, next) {
     await Post.deleteMany({});
     res.status(200).json({
       status: "success",
--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,33 @@
 var express = require("express");
 var router = express.Router();
 
+const handleErrorAsync = require("../service/handleErrorAsync");
 const PostsController = require("../controllers/posts");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  PostsController.getPosts(req, res);
+router.get("/", async function (req, res, next) {
+  PostsController.getPosts(req, res, next);
 });
-router.post("/", function (req, res, next) {
-  PostsController.createdPost(req, res);
-});
-router.patch("/:id", async function (req, res, next) {
-  PostsController.updatedPost(req, res);
-});
+router.post(
+  "/",
+  handleErrorAsync(async function (req, res, next) {
+    PostsController.createdPost(req, res, next);
+  })
+);
+router.patch(
+  "/:id",
+  handleErrorAsync(async function (req, res, next) {
+    PostsController.updatedPost(req, res, next);
+  })
+);
 router.delete("/", async function (req, res, next) {
-  PostsController.clearPosts(req, res);
+  PostsController.clearPosts(req, res, next);
 });
-router.delete("/:id", async function (req, res, next) {
-  PostsController.deletedPost(req, res);
-});
+router.delete(
+  "/:id",
+  handleErrorAsync(async function (req, res, next) {
+    PostsController.deletedPost(req, res, next);
+  })
+);
 
 module.exports = router;
